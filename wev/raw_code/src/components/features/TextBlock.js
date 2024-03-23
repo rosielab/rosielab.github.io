218666e4d1d6d41017d0d213019f91560e4014d5
Paul Jaques Gilbert Maublanc <au750967@uni.au.dk>
2024-03-23 12:38:25 +0100

    Add important dates for call for papers

--- wev/raw_code/src/components/features/TextBlock.js
+++ wev/raw_code/src/components/features/TextBlock.js
@@ -11,6 +11,10 @@ const HeadingInfoContainer = tw.div`flex flex-col items-center`;
 const HeadingDescription = tw.p`mt-4 font-medium text-gray-600 text-center `;
 const HeadingMinor = tw.p`mt-0 mb-0 font-medium text-gray-600 text-center `;
 
+const List = tw.ul`mt-4`;
+const ListItem = tw.li`mt-3 font-medium text-gray-600`;
+const Link = tw.a`border-b-2 border-transparent hocus:text-primary-500 hocus:border-primary-500 pb-1 transition duration-300`;
+
 
 const SvgDotPattern1 = tw(
   SvgDotPatternIcon
@@ -26,25 +30,43 @@ export default () => {
       <SingleColumn>
         <HeadingInfoContainer id = "cfp">
           <HeadingTitle>Call for Papers</HeadingTitle>
-          <HeadingDescription>
-          We challenge those working in behavior generation to consider the challenges of working 
-          within the constraints of embodiment and context, and those designing robots to consider 
-          the benefits of embedding state of the art generative models. We strongly encourage 
-          submissions from researchers at any stage of their career.
-          </HeadingDescription>
           <HeadingMinor>
           Papers are to be maximum 4 pages plus references and should adhere to the RSS format 
-          found <a href="https://roboticsconference.org/information/authorinfo/">HERE</a>
+          found <Link href="https://roboticsconference.org/information/authorinfo/">HERE</Link>
           </HeadingMinor>
           <HeadingMinor>
-          Papers are to be submitted via Microsoft CMT here: TBA
+          Papers are to be submitted via Microsoft CMT here: <Link href="https://cmt3.research.microsoft.com/wev2024/Submission/Index">HERE</Link>
           </HeadingMinor>
+          <HeadingDescription>
+          We aim to bring together robot designers, interaction scholars and those working in speech and 
+          sound synthesis. We challenge those working in speech synthesis and sound generation to consider 
+          the challenges of working within the constraints of embodiment and context, and those designing 
+          robots to consider the benefits of embedding state of the art generative models.
+          </HeadingDescription>
           <HeadingMinor>
-          Deadline for submission is <b>May 15th 2024</b>
+          <b>Important Dates</b>
           </HeadingMinor>
+          <List>
+            <ListItem>
+              Early-bird submission deadline: <b>April 24th, 2024</b>
+            </ListItem>
+            <ListItem>
+              Early-bird notification of acceptance: <b>May 22nd, 2024</b>
+            </ListItem>
+            <ListItem>
+              General submission deadline: <b>May 15th, 2024</b>
+            </ListItem>
+            <ListItem>
+              General notification of acceptance: <b>June 15th, 2024</b>
+            </ListItem>
+            <ListItem>
+              Camera-ready deadline: <b>July 10th, 2024</b>
+            </ListItem>
+          </List>
           <HeadingMinor>
-          Notification of acceptance is <b>June 15th 2024</b>
+          All deadlines are at 23:59 AOE.
           </HeadingMinor>
+
         </HeadingInfoContainer>
       </SingleColumn>
       <SvgDotPattern1 />
