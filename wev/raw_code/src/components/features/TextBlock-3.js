1f68da7895fc5839ec2e62d9859ac8d57adcebf3
Paul Jaques Gilbert Maublanc <pol.lom@outlook.com>
2024-04-04 16:09:26 +0200

    Add schedule table

--- wev/raw_code/src/components/features/TextBlock-3.js
+++ wev/raw_code/src/components/features/TextBlock-3.js
@@ -10,8 +10,7 @@ const SingleColumn = tw.div` max-w-screen-xl mx-auto py-20 lg:py-24`;
 const HeadingInfoContainer = tw.div`flex flex-col items-center`;
 const HeadingMinor = tw.p`mt-0 mb-0 font-medium text-gray-600 text-center `;
 
-const Table = tw.table`table-auto text-gray-600`;
-
+const Table = tw.table`table-auto text-gray-600 lg:w-2/3`;
 
 const SvgDotPattern3 = tw(
   SvgDotPatternIcon
@@ -27,100 +26,97 @@ export default () => {
       <SingleColumn>
         <HeadingInfoContainer id = "schedule">
           <HeadingTitle>Schedule</HeadingTitle>
-          <HeadingMinor>
-          TBA
-          </HeadingMinor>
-          {/*<Table>
-                      <thread>
-                        <tr>
-                          <th> Starting time </th>
-                          <th> Ending time </th>
-                          <th> Activity </th>
-                        </tr>
-                      </thread>
-                      <tbody>
-                        <tr>
-                          <td> 09:00 </td>
-                          <td> 09:15 </td>
-                          <td> Opening remarks </td>
-                        </tr>
-                        <tr>
-                          <td> 09:15 </td>
-                          <td> 09:30 </td>
-                          <td> Body movement game </td>
-                        </tr>
-                        <tr>
-                          <td> 09:30 </td>
-                          <td> 10:00 </td>
-                          <td> Invited Speaker 1 </td>
-                        </tr>
-                        <tr>
-                          <td> 10:00 </td>
-                          <td> 10:30 </td>
-                          <td> Lightning talks 1 (3-5mins + panel Q&A) </td>
-                        </tr>
-                        <tr>
-                          <td> 10:30 </td>
-                          <td> 10:45 </td>
-                          <td> Break </td>
-                        </tr>
-                        <tr>
-                          <td> 10:45 </td>
-                          <td> 11:15 </td>
-                          <td> Invited Speaker 2 </td>
-                        </tr>
-                        <tr>
-                          <td> 11:15 </td>
-                          <td> 11:45 </td>
-                          <td> Lightning talks 2 (3-5mins + panel Q&A) </td>
-                        </tr>
-                        <tr>
-                          <td> 11:45 </td>
-                          <td> 12:15 </td>
-                          <td> Invited Speaker 3 </td>
-                        </tr>
-                        <tr>
-                          <td> 12:15 </td>
-                          <td> 12:45 </td>
-                          <td> Invited Speaker 4 </td>
-                        </tr>
-                        <tr>
-                          <td> 12:45 </td>
-                          <td> 13:30 </td>
-                          <td> Lunch </td>
-                        </tr>
-                        <tr>
-                          <td> 13:30 </td>
-                          <td> 14:00 </td>
-                          <td> Invited Speaker 5 </td>
-                        </tr>
-                        <tr>
-                          <td> 14:00 </td>
-                          <td> 14:15 </td>
-                          <td> Voice task Intro </td>
-                        </tr>
-                        <tr>
-                          <td> 14:15 </td>
-                          <td> 16:00 </td>
-                          <td> Voice task </td>
-                        </tr>
-                        <tr>
-                          <td> 16:00 </td>
-                          <td> 16:15 </td>
-                          <td> Break </td>
-                        </tr>
-                        <tr>
-                          <td> 16:15 </td>
-                          <td> 16:45 </td>
-                          <td> Voice task discussion </td>
-                        </tr>
-                        <tr>
-                          <td> 16:45 </td>
-                          <td> 17:00 </td>
-                          <td> Closing remarks </td>
-                        </tr>
-                      </tbody>
-                    </Table>*/}
+          <Table>
+            <thead>
+              <tr>
+                <th> Starting time </th>
+                <th> Ending time </th>
+                <th> Activity </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td> 09:00 </td>
+                <td> 09:15 </td>
+                <td> Opening remarks </td>
+              </tr>
+              <tr>
+                <td> 09:15 </td>
+                <td> 09:30 </td>
+                <td> Body movement game </td>
+              </tr>
+              <tr>
+                <td> 09:30 </td>
+                <td> 10:00 </td>
+                <td> Invited Speaker 1 </td>
+              </tr>
+              <tr>
+                <td> 10:00 </td>
+                <td> 10:30 </td>
+                <td> Lightning talks 1 (3-5mins + panel Q&A) </td>
+              </tr>
+              <tr>
+                <td> 10:30 </td>
+                <td> 10:45 </td>
+                <td> Break </td>
+              </tr>
+              <tr>
+                <td> 10:45 </td>
+                <td> 11:15 </td>
+                <td> Invited Speaker 2 </td>
+              </tr>
+              <tr>
+                <td> 11:15 </td>
+                <td> 11:45 </td>
+                <td> Lightning talks 2 (3-5mins + panel Q&A) </td>
+              </tr>
+              <tr>
+                <td> 11:45 </td>
+                <td> 12:15 </td>
+                <td> Invited Speaker 3 </td>
+              </tr>
+              <tr>
+                <td> 12:15 </td>
+                <td> 12:45 </td>
+                <td> Invited Speaker 4 </td>
+              </tr>
+              <tr>
+                <td> 12:45 </td>
+                <td> 13:30 </td>
+                <td> Lunch </td>
+              </tr>
+              <tr>
+                <td> 13:30 </td>
+                <td> 14:00 </td>
+                <td> Invited Speaker 5 </td>
+              </tr>
+              <tr>
+                <td> 14:00 </td>
+                <td> 14:15 </td>
+                <td> Voice task Intro </td>
+              </tr>
+              <tr>
+                <td> 14:15 </td>
+                <td> 16:00 </td>
+                <td> Voice task </td>
+              </tr>
+              <tr>
+                <td> 16:00 </td>
+                <td> 16:15 </td>
+                <td> Break </td>
+              </tr>
+              <tr>
+                <td> 16:15 </td>
+                <td> 16:45 </td>
+                <td> Voice task discussion </td>
+              </tr>
+              <tr>
+                <td> 16:45 </td>
+                <td> 17:00 </td>
+                <td> Closing remarks </td>
+              </tr>
+            </tbody>
+          </Table>
         </HeadingInfoContainer>
       </SingleColumn>
       <SvgDotPattern3 />
